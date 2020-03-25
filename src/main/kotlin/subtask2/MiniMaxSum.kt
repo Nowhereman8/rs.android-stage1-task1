9ee30cbe3c6b79e771ca28c051fab3e12ef109c7
Nowhereman8 <61270422+Nowhereman8@users.noreply.github.com>
2020-03-25 10:26:48 +0200

    Add files via upload

--- src/main/kotlin/subtask2/MiniMaxSum.kt
+++ src/main/kotlin/subtask2/MiniMaxSum.kt
@@ -2,8 +2,34 @@ package subtask2
 
 class MiniMaxSum {
 
-    // TODO: Complete the following function
+
     fun getResult(input: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        // val mylist = input.toCollection(ArrayList())
+        var minlist = input.toCollection(ArrayList())
+        var maxlist = input.toCollection(ArrayList())
+       var min = maxlist.get(0)
+       // println(minlist.size)
+       var max = minlist.get(0)
+        //println(max)
+        var minsum = 0
+        var maxsum = 0
+
+        for (i in 0..minlist.size -1) {
+            if (max < minlist.get(i)) { max = minlist.get(i) }
+        }
+        for (i in 0..maxlist.size -1) {
+            if (min > maxlist.get(i)) {min = maxlist.get(i) }
+        }
+        minlist.remove(max)
+        println(minlist)
+        maxlist.remove(min)
+        println(maxlist)
+        for (i in 0..minlist.size - 1) {
+            maxsum += minlist.get(i)
+        }
+        for (i in 0..maxlist.size - 1) {
+            minsum += maxlist.get(i)
+        }
+        return intArrayOf(maxsum, minsum)
     }
 }
