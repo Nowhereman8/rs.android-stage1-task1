a6d442448b327d53306d48a81b5a530cc2d2fde6
Nowhereman8 <61270422+Nowhereman8@users.noreply.github.com>
2020-03-27 21:08:16 +0200

    Add files via upload

--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -2,8 +2,62 @@ package subtask4
 
 class StringParser {
 
-    // TODO: Complete the following function
+
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        var temp = ""
+        var t : Int
+        var count = 0
+        val res = arrayListOf<String>()
+
+        for (i in 0..inputString.length-1) {
+            when (inputString[i].toChar()) {
+                '[' -> {
+                    count++
+                    t = i + 1
+                    while (count != 0) {
+                        if (count != 0 ) {
+                            temp += inputString[t].toChar()
+                        }
+                        t++
+                        if (inputString[t].toChar() == '[') {count++}
+                        if (inputString[t].toChar() == ']') {count--}
+                    }
+                    res.add(temp)
+                    // println("fin $temp")
+                    temp = ""
+                }
+                '(' -> {
+                    count++
+                    t = i + 1
+                    while (count != 0) {
+                        if (count != 0 ) {
+                            temp += inputString[t].toChar()
+                        }
+                        t++
+                        if (inputString[t].toChar() == '(') {count++}
+                        if (inputString[t].toChar() == ')') {count--}
+                    }
+                    res.add(temp)
+                    // println("fin $temp")
+                    temp = ""
+                }
+                '<' -> {
+                    count++
+                    t = i + 1
+                    while (count != 0) {
+                        if (count != 0 ) {
+                            temp += inputString[t].toChar()
+                        }
+                        t++
+                        if (inputString[t].toChar() == '<') {count++}
+                        if (inputString[t].toChar() == '>') {count--}
+                    }
+                    res.add(temp)
+                    temp = ""
+                }
+            }
+        }
+
+        return res.toTypedArray()
     }
 }
